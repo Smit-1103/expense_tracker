0e9388a598c4ed3dda48fca1d1eac79ffbedcebf
smit patel <smitpatel7032@gmail.com>
2024-01-23 18:14:38 +0530

    chore: optimize code

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -7,7 +7,6 @@ import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../main.dart';
 
@@ -37,6 +36,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpensesOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => FractionallySizedBox(
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:expense_tracker/constants.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -33,15 +36,39 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
-//validation of the input fields
-  void _submitExpenseData() {
-    final enteredAmount =
-        double.tryParse(_amountController.text); // convert the text into int
-    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
-    // validation of the entered information
-    if (_titleController.text.trim().isEmpty ||
-        amountIsInvalid ||
-        _selectedDate == null) {
+  void _showDialog() {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (ctx) => CupertinoAlertDialog(
+          title: const Text(
+            'Invalid Input',
+            style: TextStyle(
+              color: MyColors.errorColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          content: const Text(
+            'Please make sure a valid title, amount, date, and category is inserted.',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(MyColors.errorColor),
+                overlayColor:
+                    MaterialStateProperty.resolveWith<Color>((states) {
+                  return MyColors.errorLightColor;
+                }),
+              ),
+              child: const Text('Okay'),
+            ),
+          ],
+        ),
+      );
+    } else {
       showDialog(
         context: context,
         builder: (ctx) => AlertDialog(
@@ -74,6 +101,19 @@ class _NewExpenseState extends State<NewExpense> {
               borderRadius: BorderRadius.circular(15)), // Rounded corners
         ),
       );
+    }
+  }
+
+//validation of the input fields
+  void _submitExpenseData() {
+    final enteredAmount =
+        double.tryParse(_amountController.text); // convert the text into int
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    // validation of the entered information
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      _showDialog();
       return;
       // after thhis return no other code is executed further
     }
@@ -98,201 +138,204 @@ class _NewExpenseState extends State<NewExpense> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 26, 16, 16),
-      child: Column(
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Add new expense',
-              style: Theme.of(context).textTheme.titleLarge,
+    final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(16, 26, 16, keyboardSpace + 16),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Add new expense',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
             ),
-          ),
-          const Divider(
-            color: MyColors.textColor,
-            thickness: 1,
-            height: 32,
-          ),
-          TextField(
-            controller: _titleController,
-            maxLength: 50,
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              prefixIcon: const Icon(Icons.add_box_outlined),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(80.0),
-                borderSide: const BorderSide(
-                  width: 1.5,
-                ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: const BorderSide(
-                  color: MyColors.primaryColor,
-                  width: 2.0,
-                ),
-              ),
-              hintText: 'Enter the expense title',
-              hintStyle: const TextStyle(
-                fontSize: 14.0,
-              ),
-              contentPadding: const EdgeInsets.all(10),
+            const Divider(
+              color: MyColors.textColor,
+              thickness: 1,
+              height: 32,
             ),
-          ),
-          const SizedBox(
-            height: 3,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: _amountController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    // prefixText: '₹ ',
-                    prefixIcon: const Icon(Icons.currency_rupee_rounded),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(80.0),
-                      borderSide: const BorderSide(
-                        width: 1.5,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: const BorderSide(
-                        color: MyColors.primaryColor,
-                        width: 2.0,
-                      ),
-                    ),
-                    hintText: 'Enter the amount',
-                    hintStyle: const TextStyle(
-                      fontSize: 14.0,
-                    ),
-                    contentPadding: const EdgeInsets.all(10),
+            TextField(
+              controller: _titleController,
+              maxLength: 50,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.add_box_outlined),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(80.0),
+                  borderSide: const BorderSide(
+                    width: 1.5,
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                child: OutlinedButton.icon(
-                  onPressed: _presentDatePicker,
-                  icon: const Icon(
-                    Icons.calendar_month,
-                  ),
-                  label: Text(
-                    _selectedDate == null
-                        ? 'Select Date'
-                        : formatter.format(_selectedDate!),
-                  ),
-                  style: OutlinedButton.styleFrom(
-                    padding: const EdgeInsets.all(12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(80.0),
-                    ),
-                    backgroundColor: Colors
-                        .transparent, // Set background color to transparent
-                    foregroundColor: const Color.fromARGB(
-                        255, 4, 79, 141), // Set the text color to blue
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(
+                    color: MyColors.primaryColor,
+                    width: 2.0,
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          DropdownButtonFormField<Category>(
-            value: _selectedCategory,
-            items: Category.values
-                .map((category) => DropdownMenuItem(
-                      value: category,
-                      child: Row(
-                        children: [
-                          Icon(categoryIcons[
-                              category]), // Display icon for each category
-                          const SizedBox(
-                              width: 10), // Add spacing between icon and text
-                          Text(
-                            category.name.toString(),
-                          ),
-                        ],
-                      ),
-                    ))
-                .toList(),
-            onChanged: (value) {
-              setState(() {
-                _selectedCategory = value as Category;
-              });
-            },
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(80.0),
-                borderSide: const BorderSide(
-                  width: 1.5,
+                hintText: 'Enter the expense title',
+                hintStyle: const TextStyle(
+                  fontSize: 14.0,
                 ),
+                contentPadding: const EdgeInsets.all(10),
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: const BorderSide(
-                  color: MyColors.primaryColor,
-                  width: 2.0,
-                ),
-              ),
-              contentPadding: const EdgeInsets.all(10),
             ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Row(
-            children: [
-              const Spacer(),
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all(MyColors.errorColor),
-                  overlayColor:
-                      MaterialStateProperty.resolveWith<Color>((states) {
-                    return MyColors.errorLightColor;
-                  }),
-                ),
-                child: const Text(
-                  'Cancel',
-                  style: TextStyle(
-                    fontSize: 14,
+            const SizedBox(
+              height: 3,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      // prefixText: '₹ ',
+                      prefixIcon: const Icon(Icons.currency_rupee_rounded),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(80.0),
+                        borderSide: const BorderSide(
+                          width: 1.5,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          color: MyColors.primaryColor,
+                          width: 2.0,
+                        ),
+                      ),
+                      hintText: 'Enter the amount',
+                      hintStyle: const TextStyle(
+                        fontSize: 14.0,
+                      ),
+                      contentPadding: const EdgeInsets.all(10),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 8), // Adjust the space between buttons
-              OutlinedButton(
-                onPressed: _submitExpenseData,
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      const Color.fromARGB(255, 23, 56, 107)),
-                  foregroundColor:
-                      MaterialStateProperty.all(MyColors.backgroundColor),
-                  overlayColor:
-                      MaterialStateProperty.resolveWith<Color>((states) {
-                    return MyColors.accentColor;
-                  }),
-                  side: MaterialStateProperty.all(BorderSide.none),
+                const SizedBox(
+                  width: 10,
                 ),
-                child: const Text(
-                  'Save Expense',
-                  style: TextStyle(
-                    fontSize: 14,
+                Expanded(
+                  child: OutlinedButton.icon(
+                    onPressed: _presentDatePicker,
+                    icon: const Icon(
+                      Icons.calendar_month,
+                    ),
+                    label: Text(
+                      _selectedDate == null
+                          ? 'Select Date'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      padding: const EdgeInsets.all(12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0),
+                      ),
+                      backgroundColor: Colors
+                          .transparent, // Set background color to transparent
+                      foregroundColor: const Color.fromARGB(
+                          255, 4, 79, 141), // Set the text color to blue
+                    ),
                   ),
                 ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            DropdownButtonFormField<Category>(
+              value: _selectedCategory,
+              items: Category.values
+                  .map((category) => DropdownMenuItem(
+                        value: category,
+                        child: Row(
+                          children: [
+                            Icon(categoryIcons[
+                                category]), // Display icon for each category
+                            const SizedBox(
+                                width: 10), // Add spacing between icon and text
+                            Text(
+                              category.name.toString(),
+                            ),
+                          ],
+                        ),
+                      ))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  _selectedCategory = value as Category;
+                });
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(80.0),
+                  borderSide: const BorderSide(
+                    width: 1.5,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(
+                    color: MyColors.primaryColor,
+                    width: 2.0,
+                  ),
+                ),
+                contentPadding: const EdgeInsets.all(10),
               ),
-            ],
-          )
-        ],
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                const Spacer(),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all(MyColors.errorColor),
+                    overlayColor:
+                        MaterialStateProperty.resolveWith<Color>((states) {
+                      return MyColors.errorLightColor;
+                    }),
+                  ),
+                  child: const Text(
+                    'Cancel',
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8), // Adjust the space between buttons
+                OutlinedButton(
+                  onPressed: _submitExpenseData,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 23, 56, 107)),
+                    foregroundColor:
+                        MaterialStateProperty.all(MyColors.backgroundColor),
+                    overlayColor:
+                        MaterialStateProperty.resolveWith<Color>((states) {
+                      return MyColors.accentColor;
+                    }),
+                    side: MaterialStateProperty.all(BorderSide.none),
+                  ),
+                  child: const Text(
+                    'Save Expense',
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
